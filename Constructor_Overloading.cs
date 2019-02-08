238513b7dcbe5048ecb866bf6d8cacaf0e2ea2d9
richa2512 <41030592+richa2512@users.noreply.github.com>
2019-02-08 13:19:47 +0530

    Constructor overloading

---
+++ Constructor_Overloading.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_2_
+{
+    class Program
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        String name, branch;
+        int enroll;
+        Program(String Stname)
+        {
+            name = Stname;
+            Console.WriteLine("1st Constructor:");
+            Console.WriteLine("Student Name is "+Stname);
+        }
+        Program(String Stname,String Stbranch)
+        {
+            name = Stname;
+            branch = Stbranch;
+            Console.WriteLine("2nd Constructor:");
+            Console.WriteLine(Stname+" is in "+Stbranch+" branch");
+        }
+        Program(String Stname, String Stbranch ,int Stenroll)
+        {
+            name = Stname;
+            branch = Stbranch;
+            enroll = Stenroll;
+            Console.WriteLine("3rd Constructor:");
+            Console.WriteLine(Stname + " is in " + Stbranch+" having "+Stenroll+" Enrollment ");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program("richa");
+            Program p1 = new Program("richa","Computer");
+            Program p2 = new Program("richa","Computer",51);
+            Console.ReadLine();
+
+
+        }
+    }
+}
